7a3f74e8d0ddfb08d43abc217c60dc72b01bf9a6
orieldar <orieleldar@gmail.com>
2020-03-28 19:39:25 +0300

    WSE1

--- js/index.js
+++ js/index.js
@@ -5,3 +5,25 @@ $(function(){
         alert("emoji");
     });
 });
+
+var isScrolling;
+
+// Get the navbar
+var navbar = document.getElementById("nav");
+
+window.addEventListener('scroll', function ( event ) {
+
+    // Clear our timeout throughout the scroll
+    window.clearTimeout( isScrolling );
+    navbar.classList.add("transparent")
+    navbar.classList.remove("transparent-out");
+    // Set a timeout to run after scrolling ends
+    isScrolling = setTimeout(function() {
+
+        // Run the callback
+        navbar.classList.remove("transparent");
+        navbar.classList.add("transparent-out");
+
+    }, 90);
+
+});
